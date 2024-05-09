8d6b534e28601801ed4f57cad1e1970d1b53207b
TobiasSimoni <tobiassimoni22@gmail.com>
2024-05-09 14:15:43 -0300

    Modifique el home page

--- vite-project/src/views/HomePage.jsx
+++ vite-project/src/views/HomePage.jsx
@@ -4,18 +4,57 @@ import Card from "../components/Card";
 
 const HomePage = () => {
     return (
-      <div className="p-5">
-        
-        <h1>Home Page</h1>
-        <h2>Lema de la marca</h2>
+      <div>
       
+      
+      
+           
+        <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+  <div class="container-fluid mx-5">
+    <a class="navbar-brand" href="#">Home Page</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active " aria-current="page" href="#">Crear Cuenta</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Iniciar Sesion</a>
+        </li>
+       
+       
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2 bg-primary" type="search" placeholder="Lema de la marca" aria-label="Search" />
+        <button class="btn btn-primary" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
+      
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
 
-        <button  className="btn btn-primary" >Crear Cuenta</button>
-        <button className="btn btn-primary">Iniciar Sesion</button>
+        
        
-        <p>This is the home page.</p>
-        <a href="/admin" className="btn btn-primary">ir a Admin</a>
+       
         
         <Card/>
         
