5e6fa887874fe8350a5c0526bfd4273bb75d2a8a
Juan C. Triventi <jctriventi@ikeasistencia.com.ar>
2024-05-13 20:36:27 -0300

    agrego imagen al card

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -1,28 +1,21 @@
 import React from 'react'
 
-const Card = ({item}) => {
- 
-   const {nombre,descripcion,rutasimagenes} = item
+const Card = ({ item }) => {
+  const { nombre, descripcion, rutasImagenes } = item
 
-   
+  const primeraImagenURL = rutasImagenes[0];
+
   return (
-    
-
-  <div class="col">
-<div class ="card h100">
-  <a href={"/detalles/"+ item.id}><img src={rutasimagenes}  class = "card-image-top"alt="..." /></a>
-<div class="card-body">
-      <h2 class="card-title">{nombre}</h2>
-      <p class = "card-text">{descripcion} 
-      </p>
+    <div className="col">
+      <div className="card h100">
+        <img src={primeraImagenURL} className="card-img-top" alt={nombre} />
+        <div className="card-body">
+          <h2 className="card-title">{nombre}</h2>
+          <p className="card-text">{descripcion}</p>
+        </div>
       </div>
-
-      </div>
-    
     </div>
-   
-  
-  
   )
 }
+
 export default Card
