287128dc7a16adc101dcee7551e292e333f847f5
Juan C. Triventi <jctriventi@ikeasistencia.com.ar>
2024-06-03 21:01:55 -0300

    agrego datepicker para las fechas y adapto las peticiones de categoria y caracteristicas al nuevo back end

--- vite-project/src/components/Caracteristica.jsx
+++ vite-project/src/components/Caracteristica.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { createCaracteristica } from '../interceptors/caracteristica.interceptor';
 
 const Caracteristica = () => {
   const [nombre, setNombre] = useState('');
-  const [tipo, setTipo] = useState(1); // Por defecto, el tipo es 1, pero puedes cambiarlo según tus necesidades
+  const [icono, setIcono] = useState(''); 
 
   const handleNombreChange = (e) => {
     setNombre(e.target.value);
   };
 
-  const handleTipoChange = (e) => {
-    setTipo(parseInt(e.target.value)); // Aseguramos que el tipo sea un número entero
+  const handleIconoChange = (e) => {
+    setIcono(e.target.value); 
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const newCaracteristica = { nombre, tipo }; // Creamos el objeto de característica con nombre y tipo
+      const newCaracteristica = { nombre, icono };
       await createCaracteristica(newCaracteristica);
       alert('Característica creada con éxito');
       setNombre('');
-      setTipo(1); // Reiniciamos el tipo al valor por defecto
+      setIcono('');
     } catch (error) {
       console.error('Ocurrió un error al registrar una nueva característica:', error);
       alert('Error al crear la característica. Intenta nuevamente.');
@@ -49,16 +49,15 @@ const Caracteristica = () => {
                 />
               </div>
               <div className="mb-3">
-                <label htmlFor="tipo" className="form-label">Tipo</label>
-                <select
-                  className="form-select"
-                  id="tipo"
-                  value={tipo}
-                  onChange={handleTipoChange}
+                <label htmlFor="icono" className="form-label">Icono</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="icono"
+                  value={icono}
+                  onChange={handleIconoChange}
                   required
-                >
-                  <option value="1">Tipo 1</option> {/* Aquí puedes agregar más opciones de tipo según tus necesidades */}
-                </select>
+                />
               </div>
               <button type="submit" className="btn btn-custom-green">Crear</button>
             </form>
--- vite-project/src/components/NavBar.jsx
+++ vite-project/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import LogoImage from '/Proyecto Integrador Equipo 8.png';
 import BgNavBar from '/pexels-tomfisk-1518723.jpg';
 import { logoutUser } from '../interceptors/auth.interceptor';
+import DateRangePicker from './DateRangePicker'; // Importa el componente
 
 function CustomNavbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('user') !== null);
@@ -26,18 +27,15 @@ function CustomNavbar() {
     }}>
       <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container">
-          {/* Contenedor para el logo y el texto */}
           <div className="d-flex align-items-center">
             <a className="navbar-brand text-white fw-semibold d-flex align-items-center" href="/">
               <img src={LogoImage} alt="Logo" style={{ width: '80px'}} /> 
               <span id='nav-title' className="nav-link text-custom-orange">GOTravel</span> 
             </a>
           </div>
-
           <button className="navbar-toggler navbar-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav">
               <li className="nav-item">
@@ -72,7 +70,6 @@ function CustomNavbar() {
         </div>
       </nav>
       <p className='lead-navbar text-center text-light d-none d-md-flex'>Encuentra instantáneamente las mejores ofertas en hoteles, glampings, hostales y mucho más!</p>
-      {/* Contenedor para el formulario de filtros */}
       <div className="container mt-3 mb-2">
         <form className="d-flex flex-column flex-sm-row justify-content-center align-items-center">
           <select className="form-select mb-2 mb-sm-0 me-0 me-sm-2 text-green p-2" style={{ maxWidth: '250px' }}>
@@ -82,19 +79,8 @@ function CustomNavbar() {
             <option value="tokyo">Tokio</option>
             <option value="london">Londres</option>
           </select>
-          <input
-            type="date"
-            className="form-control mb-2 mb-sm-0 me-0 me-sm-2 text-green p-2"
-            style={{ maxWidth: '250px' }}
-            placeholder="Check-in"
-          />
-          <input
-            type="date"
-            className="form-control mb-2 mb-sm-0 me-0 me-sm-2 text-green p-2 w-100"
-            style={{ maxWidth: '250px' }}
-            placeholder="Check-out"
-          />
-          <button type="submit" className="boton-filtros-nav btn btn-custom-orange p-md-2">Buscar &rarr;</button>
+          <DateRangePicker /> 
+          <button type="submit" className="boton-filtros-nav btn btn-custom-orange p-md-2 ms-2 mt-2 mt-md-0">Buscar &rarr;</button>
         </form>
       </div>
     </div>
